842b00b55a11eeffd688b4b75ee694085f324183
Leo Moraes <developerleo.it@gmail.com>
2022-03-23 09:50:02 -0300

    Exercice resolution - 42

--- Curso em vídeo/Mundo 1/python_exercicios/exe042.py
+++ Curso em vídeo/Mundo 1/python_exercicios/exe042.py
@@ -14,7 +14,7 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input("Terceiro segmento: "))
 
-# Verificando se forman um triângulo
+# Verificando as linhas formam um triângulo
 if r1 < r2 + r3 and r2< r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
     if r1 == r2 and r2 == r3:
