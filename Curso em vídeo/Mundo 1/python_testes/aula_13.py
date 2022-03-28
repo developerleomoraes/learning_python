d006b8a058b0f3887a57501f00bf1e3702e9f10d
Leo Moraes <developerleo.it@gmail.com>
2022-03-28 02:30:25 -0300

    New python exercises

---
+++ Curso em vídeo/Mundo 1/python_testes/aula_13.py
@@ -0,0 +1,16 @@
+# Estudando for em python
+
+'''i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input("Passo: "))
+
+for c in range(i, f+1, p):
+    print(c)
+print('Fim')'''
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor:'))
+    s = s + n
+print('O somatório de todos os valores foi {}'.format(s))
+
