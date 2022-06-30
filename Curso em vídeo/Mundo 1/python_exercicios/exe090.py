d9c26380fd6fa8b55e04e0359094ba73623b5163
Leo Moraes <developerleo.it@gmail.com>
2022-06-30 00:24:03 -0300

    Exercise resolution

--- Curso em vídeo/Mundo 1/python_exercicios/exe090.py
+++ Curso em vídeo/Mundo 1/python_exercicios/exe090.py
@@ -1,16 +1,17 @@
-'''Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+'''Faça um programa que leia Nome e Média de um aluno, guardando também a Situação em um dicionário.
  No final, mostre o conteúdo da estrutura na tela.'''
  
 aluno = {}
-aluno['nome'] = str(input('Nome: '))
-aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
-
-if aluno['média'] >= 7:
-     aluno['situação'] = 'Aprovado'
-elif aluno['média'] >= 5 and aluno['média'] < 7:
-     aluno['situação'] = 'Em Recuperação'
+aluno['Nome']  = str(input('Nome: '))
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+if aluno['Média'] >= 7:
+     aluno['Situação'] = 'Aprovado!'
+elif aluno['Média'] >= 5 and aluno['Média'] < 7:
+     aluno['Situação'] = 'Recuperação'
 else:
-     aluno['média'] = 'Aprovado!'
+     aluno['Situação'] = 'Reprovado!'
      
+print('-' * 30)
+
 for key, value in aluno.items():
-    print(f'{key} é igual a {value}')
+     print(f'- {key} é igual a {value}')
